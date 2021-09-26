7b5ae4a26ca5d1544e91698a4be54e8a1a6914d1
tiagoenriquez <tiagoenriquez@gmail.com>
2021-09-25 23:06:20 -0300

    corrigindo método de obtenção de nome de usuário

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -47,7 +47,7 @@ class Pessoa
         $primeiroNome = $this->obterPrimeiroNome($nome);
         $ultimoNome = $this->obterUltimoNome($nome);
         $usuarioAcentuado = strtolower($primeiroNome . "." . $ultimoNome . $indice);
-        return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$usuarioAcentuado);
+        return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","ç","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A c e E i I o O u U n N"),$usuarioAcentuado);
     }
 
     private function obterEmail($usuario)
